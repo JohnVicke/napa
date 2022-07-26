ec12f45e4bb66a6c1877bc6275fd398bb921c8d5
Viktor Malmedal <viktormalmedal@gmail.com>
2022-07-26 21:44:48 +0200

    add google signin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
+import Link from "next/link";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
+  const session = useSession();
   const hello = trpc.useQuery(["example.hello", { text: "from time keeper" }]);
 
   return (
@@ -15,6 +18,12 @@ const Home: NextPage = () => {
 
       <main className="container mx-auto flex flex-col items-center justify-center h-screen p-4">
         {!hello.isLoading && hello?.data && <h1>{hello.data.greeting}</h1>}
+        {session.status === "loading" ||
+          (session.status === "unauthenticated" ? (
+            <Link href="/api/auth/signin">Sign in</Link>
+          ) : (
+            <div>Welcome back {session.data?.user?.name}</div>
+          ))}
       </main>
     </>
   );
