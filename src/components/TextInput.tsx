34b25ed335b4edfde1ee223b10b2c9f6a8db1896
Viktor Malmedal <viktormalmedal@gmail.com>
2022-07-28 19:48:16 +0200

    fix: add padding to icon in textfield

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,11 +1,5 @@
 import React from "react";
 
-const topPaddingMap = new Map([
-  ["sm", 1],
-  ["md", 3],
-  ["lg", 8],
-]);
-
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   startIcon?: React.ReactNode;
   endContent?: React.ReactNode;
@@ -18,8 +12,6 @@ export const TextInput = ({
   containerSize = "md",
   ...props
 }: InputProps) => {
-  const paddingTop = topPaddingMap.get(containerSize);
-  console.log(paddingTop);
   return (
     <div className="relative">
       <div className="absolute top-[50%] translate-y-[-50%] left-2 gap-1 hidden lg:flex">
@@ -27,9 +19,9 @@ export const TextInput = ({
       </div>
       <input
         {...props}
-        className={`input input-${containerSize} pl-${
+        className={`input input-${containerSize}  input-bordered w-64 pl-${
           !!startIcon ? 8 : 4
-        } input-bordered w-64 hidden lg:block`}
+        } hidden lg:block`}
       />
       <div className="absolute top-[50%] translate-y-[-50%] right-2 gap-1 hidden lg:flex">
         {endContent}
