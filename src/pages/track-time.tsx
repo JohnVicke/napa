f911d9926e46b37775ba1f2fcce7182b49cf9f35
Viktor Malmedal <viktormalmedal@gmail.com>
2022-07-28 21:30:22 +0200

    feat: wip track-time

--- src/pages/track-time.tsx
+++ src/pages/track-time.tsx
@@ -1,9 +1,53 @@
 import React from "react";
+import { TextInput } from "../components/TextInput";
 
 const TrackTime = () => {
+  const [editManually, setEditManually] = React.useState(false);
+
   return (
     <div>
-      <h1>Track time</h1>
+      <div className="sticky-top flex gap-4 py-2 bg-base-100 items-end">
+        <TextInput
+          label="Description"
+          placeholder="Work..."
+          className="w-full"
+        />
+        {!editManually ? (
+          <>
+            <button className="btn btn-primary">
+              <i className="ri-play-fill font-normal mr-2" />
+              Start timer
+            </button>
+            <button
+              className="btn btn-outline"
+              onClick={() => setEditManually(true)}
+            >
+              <i className="ri-edit-2-fill font-normal mr-2" />
+              Enter manually
+            </button>
+          </>
+        ) : (
+          <>
+            <TextInput label="from" type="time" value="08:00" />
+            <TextInput label="to" type="time" value="17:00" />
+            <button className="btn btn-primary">
+              <i className="ri-save-fill font-normal mr-2" />
+              Save
+            </button>
+            <button
+              className="btn btn-outline"
+              onClick={() => setEditManually(false)}
+            >
+              <i className="ri-time-fill font-normal mr-2" />
+              Use timer
+            </button>
+          </>
+        )}
+      </div>
+      <div className="h-96">ashdf</div>
+      <div className="h-96">ashdf</div>
+      <div className="h-96">ashdf</div>
+      <div className="h-96">ashdf</div>
     </div>
   );
 };
