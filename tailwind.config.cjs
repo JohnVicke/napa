f4903060d819b5f1d9c1698a0063a67cb57d8f6f
Viktor Malmedal <38523983+JohnVicke@users.noreply.github.com>
2022-07-28 20:26:32 +0200

    feat: update dark theme (#5)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,4 +5,22 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      "light",
+      {
+        dark: {
+          primary: "#8ebbff",
+          secondary: "#EAB0E7",
+          accent: "#1FB2A6",
+          neutral: "#2F3855",
+          "base-100": "#24293e",
+          info: "#8A99EC",
+          success: "#A7F2A0",
+          warning: "#F3F1B7",
+          error: "#F39C9C",
+        },
+      },
+    ],
+  },
 };
