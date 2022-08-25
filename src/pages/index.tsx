4702b76960fe88e021bb88bf872ce80446b395d5
Viktor Malmedal <viktormalmedal@gmail.com>
2022-08-25 09:16:27 +0200

    add simple landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import type { GetServerSideProps, NextPage } from "next";
-import Head from "next/head";
-import { trpc } from "../utils/trpc";
-import { getAuthSession } from "../utils/getAuthSession";
 import { signIn } from "next-auth/react";
+import Head from "next/head";
+import Image from "next/image";
+import landingImage from "public/images/landing-image.webp";
+import { getAuthSession } from "../utils/getAuthSession";
 
 const Home: NextPage = () => {
   return (
@@ -15,14 +16,38 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <h1 className="text-5xl font-bold">napa</h1>
-        <div className="py-6" />
-        <button
-          className="btn btn-primary float-right lg:float-left"
-          onClick={() => signIn()}
-        >
-          Get Started
-        </button>
+        <div className="hero ">
+          <div className="hero-content flex-col md:flex-row">
+            <Image
+              src={landingImage}
+              objectFit="cover"
+              height={2000}
+              className="max-w-sm rounded-lg shadow-2xl"
+              alt="hero image"
+            />
+            <div>
+              <h1 className="text-4xl font-bold">
+                Not Another Productivity App
+              </h1>
+              <div className="pt-5" />
+              <p className="leading-relaxed tracking-wide">
+                In order to deliver a good work-life balance, we track the
+                amount of time you spend working throughout your
+                workweek—whether it’s at an office or home on your laptop. It
+                all adds up so you can plan for the upcoming week and make sure
+                you have time to relax and play too, without worrying about how
+                much time you allocated to each task.
+              </p>
+              <div className="py-6" />
+              <button
+                className="btn btn-primary float-left"
+                onClick={() => signIn()}
+              >
+                Get Started
+              </button>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
